187e9de286c08bb74dac1e02e11f35126a8c5643
Asadbek <xamidovikromjon007@gmail.com>
2023-01-10 19:48:04 +0500

    Update settings.py

--- confige/settings.py
+++ confige/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lc@4+%@f=s2dmy)+seyb@(=mv*q@ik9s6)qae1^rl3ncpqoxc$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*', '198.211.99.20', 'localhost', '127.0.0.1']
 
 
 
